af394e93f7241ff28d24f69f61dbfe21f868159e
hronovv <ivanonlane@gmail.com>
2026-09-22 22:30:48 +0300

    wrapping map[string]string in a struct as a base

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+type Store struct {
+	data map[string]string
+}
+
+func NewStore() *Store {
+	return &Store{
+		data: make(map[string]string),
+	}
+}
+
+func (s *Store) Get(key string) (string, bool) {
+	v, ok := s.data[key]
+	return v, ok
+}
+
+func (s *Store) Set(k, v string) {
+	s.data[k] = v
+}
+
+func main() {
+	store := NewStore()
+	store.Set("lol", "32")
+	v, ok := store.Get("lol")
+	fmt.Println(v, ok)
 }
